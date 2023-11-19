fcd80bfefa18a9dd38bde02ca8cb08b4be3441dc
penkovskiy1 <penkovskii02@gmail.com>
2023-11-19 17:05:59 +0300

    First commit

---
+++ app/src/main/java/com/example/practika222/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.practika222
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Base_Theme_Practika222)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Practika22.2"
+include(":app")
+ 
